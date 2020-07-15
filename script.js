22dcc595abf089746ff15f11c7ff98e04cbd8725
Adrian S MUresan <adriansmuresan@gmail.com>
2020-07-15 16:20:13 -0500

    Selects all the input form the form, adds the first name if the input is blank

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const form = document.getElementById('form');
 
-form.addEventListener('submit'), e => {
-  e.preventDefault();
 
-}
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  const firstName =  form['firstname'].value;
+  const lastName = form['lastname'].value;
+  const email = form['email'].value;
+  const password = form['password'].value;
+ 
+  if (firstName === '') {
+    const small = form['firstname'].parentNode.querySelector('small');
+    small.innerText = 'First Name cannot be empty';
+    small.style.opacity = 1;
+  }
+  
+});
